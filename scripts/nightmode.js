2d9d8fa4743b57115d0df1503c1acf2c1202db73
Whyiest <74537743+Whyiest@users.noreply.github.com>
2022-11-26 14:37:43 -0500

    Adding nightmode

---
+++ scripts/nightmode.js
@@ -0,0 +1,9 @@
+header("javascript")
+alert("test");
+function changeToWhite () {
+    var element = document.getElementById("home");
+    element.style.backgroundColor = "#FFFFFF";
+    document.write.innerHTML("Hello-World")
+}
+
+changeToWhite();
